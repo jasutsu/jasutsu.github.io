fa12b4575c05897838d8d0d2604a38247b3e584f
Jashandeep Singh Bhullar <jashanbhullar376@gmail.com>
2024-10-14 14:49:38 +0530

    feat(project 2): click to see time of the score

--- vk2k3v5bv2nn4/scores.js
+++ vk2k3v5bv2nn4/scores.js
@@ -1,82 +1,87 @@
-var rows = document.querySelectorAll('.wrapper.scores-wrapper .scores tbody tr');
 var tbody = document.querySelector('.wrapper.scores-wrapper .scores tbody');
 
 function updateScoresTable() {
     var rows = '';
     var list = getStatsList();
-    var firstRow = true;
     list.slice().reverse().forEach(data => {
         var cells = `<td>${data.mistake}</td>`;
         cells += `<td>${data.wpm}</td>`;
         cells += `<td>${data.cpm}</td>`;
-        if (!firstRow) {
-            rows += `<tr>${cells}</tr>`;
-        }
-        firstRow = false;
+        rows += `<tr>${cells}</tr>`;
     });
 
     tbody.innerHTML = rows;
+    updateEventListeners(list);
 }
 
 function addToStatsList(mistakes, wpms, cpms) {
-	var list = getStatsList();
-	var data = {
-		'mistake': mistakes,
-		'wpm': wpms,
-		'cpm': cpms,
-		'time': getTimeObject()
-	}
-	list.push(data);
-	console.log(list);
-	localStorage.setItem('statList', JSON.stringify(list));
+    var list = getStatsList();
+    var data = {
+        'mistake': mistakes,
+        'wpm': wpms,
+        'cpm': cpms,
+        'time': getTimeObject()
+    }
+    list.push(data);
+    console.log(list);
+    localStorage.setItem('statList', JSON.stringify(list));
     updateScoresTable();
 }
 
 function deleteFromStatsList() {
-	var list = getStatsList();
-	if(list.length > 0) {
-		list.pop();
-	}
-	console.log(list);
-	localStorage.setItem('statList', JSON.stringify(list));
+    var list = getStatsList();
+    if (list.length > 0) {
+        list.pop();
+    }
+    console.log(list);
+    localStorage.setItem('statList', JSON.stringify(list));
 }
 
 function getTimeObject() {
-	var currentTime = new Date();
-	const monthNames = ["Jan", "Feb", "Mar", "Apr",
-		"May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-	var month = monthNames[currentTime.getMonth()];
-	var date = String(currentTime.getDate());
+    var currentTime = new Date();
+    const monthNames = ["Jan", "Feb", "Mar", "Apr",
+        "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var month = monthNames[currentTime.getMonth()];
+    var date = String(currentTime.getDate());
 
-	var hours = String(currentTime.getHours()).padStart(2, '0');
-	var minutes = String(currentTime.getMinutes()).padStart(2, '0');
-	var seconds = String(currentTime.getSeconds()).padStart(2, '0');
-	var timeOfDay = `${hours}:${minutes}:${seconds}`;
+    var hours = String(currentTime.getHours()).padStart(2, '0');
+    var minutes = String(currentTime.getMinutes()).padStart(2, '0');
+    var seconds = String(currentTime.getSeconds()).padStart(2, '0');
+    var timeOfDay = `${hours}:${minutes}:${seconds}`;
 
-	return {'date': `${month} ${date}`, 'timeOfDay': timeOfDay};
+    return { 'date': `${month} ${date}`, 'timeOfDay': timeOfDay };
 }
 
 function getStatsList() {
-	var list = JSON.parse(localStorage.getItem('statList')) || [];
-	return list;
+    var list = JSON.parse(localStorage.getItem('statList')) || [];
+    return list;
 }
 
-for(var i = 0; i < rows.length; i += 1) {
-    var row = rows[i];
+function updateEventListeners(list) {
+    var rows = document.querySelectorAll('.wrapper.scores-wrapper .scores tbody tr');
 
-    // row.addEventListener('mouseenter', () => {
-    //     var cells = row.querySelectorAll('td');
-    //     var list = getStatsList();
-    //     var time = list[i].time;
-    //     cells[0].innerText = time.date;
-    //     cells[1].innerText = time.timeOfDay;
-    // });
-    // row.addEventListener('mouseleave', () => {
-    //     var cells = row.querySelectorAll('td');
-    //     var list = getStatsList();
-    //     cells[0].innerText = list[i].mistake;
-    //     cells[1].innerText = list[i].wpm;
-    // });
+    for (var i = 0; i < rows.length; i += 1) {
+        let row = rows[i];
+        let data = list[rows.length-1-i];
+
+        row.addEventListener('mousedown', function() {
+            let cells = row.querySelectorAll('td');
+            let time = data.time;
+            console.log(time.date);
+            console.log(cells[0]);
+            cells[0].innerText = time.date;
+            cells[1].innerText = time.timeOfDay;
+            cells[2].innerText = '';
+        });
+        row.addEventListener('mouseup', () => {
+            let cells = row.querySelectorAll('td');
+            console.log(data.mistake);
+            console.log(cells[0]);
+            cells[0].innerText = data.mistake;
+            cells[1].innerText = data.wpm;
+            cells[2].innerText = data.cpm;
+        });
+    }
 }
 
 updateScoresTable();
