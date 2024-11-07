8914a59e8c572d4d771c3626d048938196e0d1e8
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-07 18:25:57 +0530

    feat(project 3): start implementing logic with js

--- vk2k3v5bv2nn4/script.js
+++ vk2k3v5bv2nn4/script.js
@@ -1,7 +1,29 @@
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.high-score');
+const playBoard = document.querySelector('play-board');
+const controls = document.querySelectorAll('.controls');
 
+let intervalId;
+let headX = 15, headY = 15;
+let foodX = 10, foodY = 10;
+let gameOver = false;
+const GAME_FPS = 125;
 
-
-
-const initGame = () => {
-    
+const changeDirection = (e) => {
+    console.log(e);
 };
+
+const handleGameOver = () => {
+    clearInterval(intervalId);
+    alert('Game Over! Press OK to restart...');
+    location.reload();
+};
+
+const loopGame = () => {
+    if (gameOver) {
+        return handleGameOver();
+    }
+};
+
+intervalId = setInterval(loopGame, GAME_FPS);
+document.addEventListener('keydown', changeDirection);
