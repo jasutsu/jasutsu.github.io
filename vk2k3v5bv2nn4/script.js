2a1da155cae576191160ea998738cc4a2e18b7c5
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-07 18:35:17 +0530

    feat(project 3): change directions on input

--- vk2k3v5bv2nn4/script.js
+++ vk2k3v5bv2nn4/script.js
@@ -6,11 +6,31 @@ const controls = document.querySelectorAll('.controls');
 let intervalId;
 let headX = 15, headY = 15;
 let foodX = 10, foodY = 10;
+let velocityX = 0, velocityY = 0;
 let gameOver = false;
 const GAME_FPS = 125;
 
 const changeDirection = (e) => {
-    console.log(e);
+    const key = e.key;
+    
+    if (key === 'ArrowLeft' && velocityX != 1) {
+        velocityX = -1;
+        velocityY = 0;
+    } else if (key === 'ArrowRight' && velocityX != -1) {
+        velocityX = 1;
+        velocityY = 0;
+    } else if (key === 'ArrowUp' && velocityY != 1) {
+        velocityX = 0;
+        velocityY = -1;
+    } else if (key === 'ArrowDown' && velocityY != -1) {
+        velocityX = 0;
+        velocityY = 1;
+    }
+};
+
+const changeFoodPosition = () => {
+    foodX = Math.floor(Math.random()*30)+1
+    foodY = Math.floor(Math.random()*30)+1
 };
 
 const handleGameOver = () => {
@@ -19,10 +39,13 @@ const handleGameOver = () => {
     location.reload();
 };
 
+
 const loopGame = () => {
     if (gameOver) {
         return handleGameOver();
     }
+
+    
 };
 
 intervalId = setInterval(loopGame, GAME_FPS);
