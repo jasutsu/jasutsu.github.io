c8d531e9fe96e4f7a8034865cdd1e9698d0bf440
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-07 17:29:21 +0530

    feat(project 3): finish snake game

--- vk2k3v5bv2nn4/script.js
+++ vk2k3v5bv2nn4/script.js
@@ -1,4 +1,7 @@
 const playBoard = document.querySelector('.play-board');
+const scoreElement = document.querySelector('.game-details .score');
+const highScoreElement = document.querySelector('.game-details .high-score');
+const controls = document.querySelectorAll('.controls i');
 
 let foodX = 20, foodY = 2;
 let snakeX = 15, snakeY = 15;
@@ -6,13 +9,12 @@ let velocityX = 0, velocityY = 0;
 const snakeBody = [[snakeX, snakeY]];
 let gameOver = false;
 let intervalId;
+let score = 0;
+let highScore = Number(localStorage.getItem('high-score') || 0);
+highScoreElement.innerText = `High Score: ${highScore}`;
 
 const changeDirection = (e) => {
     const key = e.key;
-
-    // default
-    velocityX = 0;
-    velocityY = 0;
 
     // Extract velocity from input
     if(key === 'ArrowLeft' && velocityX != 1) {
@@ -56,26 +58,46 @@ const initGame = () => {
     snakeY += velocityY;
     snakeBody[0] = [snakeX, snakeY];
 
-    // Game Over
+    // Game over if snake hits boundaries
     if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         gameOver = true;
     }
 
     // Snake eats food
+    let hasJustEaten = false;
     if (snakeX === foodX && snakeY === foodY) {
         snakeBody.push([foodX, foodY])
         changeFoodPosition();
+        hasJustEaten = true;
+
+        // Update current score and high score
+        score++;
+        scoreElement.innerText = `Score: ${score}`;
+        if(score > highScore) {
+            highScore = score;
+            highScoreElement.innerText = `High Score: ${highScore}`;
+            localStorage.setItem('high-score', highScore);
+        }
+        console.log(score, highScore);
     }
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX};"></div>`;
     
     // Draw the whole snake body
-    snakeBody.forEach(pos => {
-        htmlMarkup += `<div class="head" style="grid-area: ${pos[1]} / ${pos[0]};"></div>`;
+    snakeBody.forEach(([posX, posY], i) => {
+        // Game over if snake hits itself
+        if(i !== 0 && !hasJustEaten &&posX === snakeX && posY === snakeY) {
+            gameOver = true;
+        }
+
+        htmlMarkup += `<div class="head" style="grid-area: ${posY} / ${posX};"></div>`;
     });
-
+    
     playBoard.innerHTML = htmlMarkup;
-
 };
+
+controls.forEach(key => {
+    key.addEventListener('click', () => changeDirection({key: key.dataset.key}));
+});
 
 changeFoodPosition();
 intervalId = setInterval(initGame, 125);
