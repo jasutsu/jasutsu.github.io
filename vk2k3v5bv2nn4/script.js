f8314ad609d5e2378e2d34cfcda6fcad7f321021
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-08 22:22:22 +0530

    feat(project 4): add dark mode

---
+++ vk2k3v5bv2nn4/script.js
@@ -0,0 +1,23 @@
+const themeButton = document.querySelector('.theme-button i');
+
+// Toggle dark mode
+themeButton.addEventListener('click', () => {
+    const isDarkMode = document.body.classList.toggle('dark-mode');
+    let darkModeStatus = isDarkMode ? 'enabled' : 'disabled';
+    if (darkModeStatus === 'enabled') {
+        themeButton.classList.replace('uil-moon', 'uil-sun');
+    } else {
+        themeButton.classList.replace('uil-sun', 'uil-moon');
+    }
+    localStorage.setItem('dark-mode', darkModeStatus);
+});
+
+// Use local storage to get dark mode status
+let darkModeStatus = localStorage.getItem('dark-mode');
+if (darkModeStatus === 'enabled') {
+    document.body.classList.add('dark-mode');
+    themeButton.classList.replace('uil-moon', 'uil-sun');
+} else {
+    document.body.classList.remove('dark-mode');
+    themeButton.classList.replace('uil-sun', 'uil-moon');
+}
