0c3bfae14ddee0fd47ce8fa13d9a7bbbd9843588
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-08 14:25:24 +0530

    feat(project 4): small adjustments for mobile

--- vk2k3v5bv2nn4/script.js
+++ vk2k3v5bv2nn4/script.js
@@ -1,8 +1,19 @@
 // TODO - add sidebar-hidden if mobile device
 
 const menuButtons = document.querySelectorAll('.menu-button');
+const screenOverlay = document.querySelector('.screen-overlay');
+
 menuButtons.forEach(button => {
     button.addEventListener('click', () => {
         document.body.classList.toggle('sidebar-hidden');
     });
 });
+
+screenOverlay.addEventListener('click', () => {
+    document.body.classList.toggle('sidebar-hidden');
+});
+
+// For large screens, show sidebar-hidden
+if (window.innerWidth > 768) {
+    document.body.classList.remove('sidebar-hidden');
+}
