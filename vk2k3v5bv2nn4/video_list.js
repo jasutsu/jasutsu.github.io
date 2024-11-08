79f03e409203a7c5a7cc961971ec19b17dad6d32
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-08 17:59:06 +0530

    feat(project 4): fill videos list from json

--- vk2k3v5bv2nn4/video_list.js
+++ vk2k3v5bv2nn4/video_list.js
@@ -1,9 +1,19 @@
+const getRandomDuration = () => {
+    const minutes = Math.floor(Math.random()*50)+5;
+    const seconds = Math.floor(Math.random()*50)+5;
+    const timeToString = (time) => time.toString().padStart(2, '0');
+    return `${timeToString(minutes)}:${timeToString(seconds)}`;
+};
+
 const getVideoCard = (thumbnailUrl, channelName, channelLogoUrl, title, views) => {
+    const defaultChannelLogoUrl = "https://yt3.googleusercontent.com/DRtVBjk2Noax94hHqr8yCcEjhNUhHRvyzBE3qS9WWilnE1-uQQNVnQd8mdG9h_IvNZCRApZSQw=s176-c-k-c0x00ffffff-no-rj";
+    channelLogoUrl = (channelLogoUrl || defaultChannelLogoUrl);
+
     return `
     <a href="#" class="video-card">
         <div class="thumbnail-container">
             <img src="${thumbnailUrl}" alt="Thumbnail" class="thumbnail">
-            <p class="duration">${duration}</p>
+            <p class="duration">${getRandomDuration()}</p>
         </div>
         <div class="video-details-container">
             <img src="${channelLogoUrl}" alt="Channel Logo" class="icon">
@@ -15,4 +25,22 @@ const getVideoCard = (thumbnailUrl, channelName, channelLogoUrl, title, views) =
         </div>
     </a>
     `;
+};
+
+const fillVideosList = () => {
+    let videosMarkup = '';
+    video_metadata.forEach(video => {
+        videosMarkup += getVideoCard(
+            video["Thumbnail URL"],
+            video["Channel Name"],
+            video["Channel Logo"],
+            video["Title"],
+            video["Views"]
+        );
+    });
+
+    const videoList = document.querySelector('.video-list');
+    videoList.innerHTML = videosMarkup;
 }
+
+fillVideosList();
