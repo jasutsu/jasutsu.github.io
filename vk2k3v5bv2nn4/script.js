8cd8642f9102f33b8d10720966bda7e6dd2360d4
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-10 17:45:49 +0530

    feat(project 6): show country on selection

--- vk2k3v5bv2nn4/script.js
+++ vk2k3v5bv2nn4/script.js
@@ -8,9 +8,19 @@ selectButton.addEventListener('click', () => {
     wrapper.classList.toggle('active');
 });
 
+// Update name on a selection
 const updateName = (selectedLi) => {
     const selectedCountry = selectedLi.innerText;
     countryText.innerText = selectedCountry;
     wrapper.classList.remove('active');
     populateCountries(countries, selectedCountry);
+    searchInput.value = "";
+};
+
+searchInput.oninput = () => {
+    const searchedValue = searchInput.value.toLowerCase();
+    const arr = countries.filter(country => {
+        return country.toLocaleLowerCase().startsWith(searchedValue);
+    });
+    populateCountries(arr);
 };
