8ca22f8e97a7ba662b0d6438f7e25e79e11b41bb
Jashandeep Singh Bhullar <jashanbhullar376@gmail.com>
2024-11-10 14:19:06 +0530

    feat(project 6): populate countries dynamically

--- vk2k3v5bv2nn4/optionsList.js
+++ vk2k3v5bv2nn4/optionsList.js
@@ -1,12 +1,62 @@
 const countries = [
     "Argentina",
-    "India",
+    "Australia",
+    "Austria",
+    "Belgium",
+    "Brazil",
+    "Canada",
+    "Chile",
+    "China",
+    "Colombia",
+    "Czech Republic",
     "Denmark",
-    "Hungray",
+    "Egypt",
+    "England",
+    "Finland",
+    "France",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Ireland",
+    "Italy",
+    "Japan",
+    "Kenya",
+    "Mexico",
+    "Netherlands",
+    "New Zealand",
+    "Nigeria",
+    "Norway",
+    "Pakistan",
+    "Peru",
+    "Philippines",
+    "Piltover",
+    "Poland",
+    "Portugal",
+    "Russia",
+    "South Africa",
+    "South Korea",
+    "Spain",
     "Sweden",
-    "Argentina",
-    "India",
-    "Denmark",
-    "Hungray",
-    "Sweden"
-]
+    "Switzerland",
+    "Thailand",
+    "Turkey",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Vietnam"
+];
+
+const options = document.querySelector('.options');
+
+const populateCountries = (arr) => {
+    let countriesMarkup = "";
+    arr.forEach(country => {
+        countriesMarkup += `<li>${country}</li>`;
+    });
+    options.innerHTML = countriesMarkup;
+};
+
+populateCountries(countries);
