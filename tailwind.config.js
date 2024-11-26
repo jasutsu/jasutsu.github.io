889ab60355bf96b5cabfeec992ed96299331e582
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-26 12:09:04 +0530

    feat(project 2): start custom design project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,7 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			animation: {
-				tilt: "tilt 10s infinite linear",
-			},
-			keyframes: {
-				tilt: {
-					"0%, 50%, 100%": {
-						transform: "rotate(0deg)",
-					},
-					"25%": {
-						transform: "rotate(1deg)",
-					},
-					"75%": {
-						transform: "rotate(-1deg)",
-					},
-				}
-			}
+			
 		},
 	},
 	plugins: [],
