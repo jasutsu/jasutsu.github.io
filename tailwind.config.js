9ee80864bee6cc8fb9faf1e53263fe7e79454352
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-26 15:06:36 +0530

    feat(project 3): add aspect ratio in input css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: [
 		"index.html"
 	],
 	theme: {
+		screens: {
+			xs: '540px',
+			...defaultTheme.screens,
+		},
+		fontFamily: {
+			satoshi: 'Satoshi-Variable, sans-serif',
+			inter: 'Inter-Variable, sans-serif',
+		},
 		extend: {
 		},
 	},
