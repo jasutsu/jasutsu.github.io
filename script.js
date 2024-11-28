f02312b5578cdb209d2ec11e337cec319f57d7a9
Jashan-Windows <jashanbhullar376@gmail.com>
2024-11-28 15:19:33 +0530

    feat(project 7): start the website project from scratch

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const initApp = () => {
-    const burgerBtn = document.getElementById('burger-button');
-    const mobileMenu = document.getElementById('mobile-menu');
 
-    const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('flex');
-    };
 
-    burgerBtn.addEventListener('click', toggleMenu);
-    mobileMenu.addEventListener('click', toggleMenu);
-};
 
-document.addEventListener('DOMContentLoaded', initApp);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SCROLL DOWN TO LOOK AT ANSWER
+
+
+
+
+
+
+// const initApp = () => {
+//     const burgerBtn = document.getElementById('burger-button');
+//     const mobileMenu = document.getElementById('mobile-menu');
+
+//     const toggleMenu = () => {
+//         mobileMenu.classList.toggle('hidden');
+//         mobileMenu.classList.toggle('flex');
+//     };
+
+//     burgerBtn.addEventListener('click', toggleMenu);
+//     mobileMenu.addEventListener('click', toggleMenu);
+// };
+
+// document.addEventListener('DOMContentLoaded', initApp);
